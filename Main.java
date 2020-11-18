c693c60f0d509e2aa40daabb48a77c70be6e7213
Sam Vuong <42590636+sam-vuong@users.noreply.github.com>
2020-11-18 05:32:30 -0800

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package test_dep;
-
-public class Main {
-    public static void main(String[] args) {
-        SamplePieChart demo = new SamplePieChart("Comparison", "Which operating system are you using?");
-        demo.pack();
-        demo.setVisible(true);
-    }
-}
